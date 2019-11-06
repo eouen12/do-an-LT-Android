668c36ae38a90f512355a7b675ae384fab19cb46
huy21298 <huy.821183@gmail.com>
2019-11-06 13:17:59 +0700

    sua activity menu

--- App Quiz Me/app/src/main/java/com/doanltandroid/quizme/MenuActivity.java
+++ App Quiz Me/app/src/main/java/com/doanltandroid/quizme/MenuActivity.java
@@ -16,36 +16,36 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        imgThuThach     = findViewById(R.id.thuthach_menu_image);
-        imgRanking      = findViewById(R.id.xephang_menu_image);
-        imgMuaCredit    = findViewById(R.id.muacredit_menu_image);
-        imgDangXuat     = findViewById(R.id.dangxuat_menu_image);
+//        imgThuThach     = findViewById(R.id.thuthach_menu_image);
+//        imgRanking      = findViewById(R.id.xephang_menu_image);
+//        imgMuaCredit    = findViewById(R.id.muacredit_menu_image);
+//        imgDangXuat     = findViewById(R.id.dangxuat_menu_image);
 
     }
 
-    public void launchActivityThuThach(View view) {
-        setIntent(LinhVucActivity.class);
-    }
-
-    public void launchActivityRank(View view) {
-        setIntent(RankActivity.class);
-    }
-
-    public void launchActivityCredit(View view) {
-        setIntent(CreditActivity.class);
-    }
-
-    public void launchActivityMain(View view) {
-        setIntent(MainActivity.class);
-    }
-
-    public void setIntent (Class lop){
-        Intent intent = new Intent(getApplicationContext(), lop);
-        startActivity(intent);
-        //overridePendingTransition(R.anim.intent_enter,R.anim.intent_exit);
-    }
-
-    public void launchActivityProfile(View view) {
-        setIntent(ProfileActivity.class);
-    }
+//    public void launchActivityThuThach(View view) {
+//        setIntent(LinhVucActivity.class);
+//    }
+//
+//    public void launchActivityRank(View view) {
+//        setIntent(RankActivity.class);
+//    }
+//
+//    public void launchActivityCredit(View view) {
+//        setIntent(CreditActivity.class);
+//    }
+//
+//    public void launchActivityMain(View view) {
+//        setIntent(MainActivity.class);
+//    }
+//
+//    public void setIntent (Class lop){
+//        Intent intent = new Intent(getApplicationContext(), lop);
+//        startActivity(intent);
+//        //overridePendingTransition(R.anim.intent_enter,R.anim.intent_exit);
+//    }
+//
+//    public void launchActivityProfile(View view) {
+//        setIntent(ProfileActivity.class);
+//    }
 }
